a8f8399a68316b2cd323c2d48cbda63c886d57c0
obara <obara@obara-VirtualBox.(none)>
2013-01-03 23:15:36 +0900

    generated myapp.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '76586de5cc2f8721aec94fc7237d3779b44d5564f127c6366d92f2bf3a56bb7b3ce77cb39ab61d87d28e3108aa892e5b60432f19b20c6fd8276f3375dacc14c5'
